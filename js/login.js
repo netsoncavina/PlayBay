013f45b7d7820f78fb8c66d71a2badae838108fb
Netson <netsoncavina@gmail.com>
2022-05-10 21:20:19 -0300

    adição da função de validação de senha

--- js/login.js
+++ js/login.js
@@ -10,3 +10,21 @@ function togglePassword(passwordID, eyeID) {
     eye.classList.remove("fa-eye-slash");
   }
 }
+
+function strongPasswordValidate(password) {
+  let strongRegex = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  );
+  return strongRegex.test(password);
+}
+
+function testPassword(passwordID) {
+  let password = document.getElementById(passwordID).value;
+  let strongPassword = strongPasswordValidate(password);
+  let passwordError = document.getElementById(`${passwordID}-error`);
+  if (strongPassword) {
+    passwordError.style.display = "none";
+  } else {
+    passwordError.style.display = "block";
+  }
+}
